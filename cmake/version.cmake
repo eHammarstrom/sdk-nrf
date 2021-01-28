f86800f87bc6592303ad16be39e1ab6ec44c09f2
Emil Hammarstrom <emil.hammarstrom@assaabloy.com>
2021-01-28 09:10:54 +0100

    Remove absolute nrf folder structure
    * Also allow removing NCS version generation if not using NCS
    
    Signed-off-by: Emil Hammarstrom <emil.hammarstrom@assaabloy.com>

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,6 +1,8 @@
-math(EXPR NCS_VERSION_CODE "(${NCS_VERSION_MAJOR} << 16) + (${NCS_VERSION_MINOR} << 8)  + (${NCS_VERSION_PATCH})")
+if(${USE_NCS_VERSION_CODE})
+        math(EXPR NCS_VERSION_CODE "(${NCS_VERSION_MAJOR} << 16) + (${NCS_VERSION_MINOR} << 8)  + (${NCS_VERSION_PATCH})")
 
-# to_hex is made available by ${ZEPHYR_BASE}/cmake/hex.cmake
-to_hex(${NCS_VERSION_CODE} NCS_VERSION_NUMBER)
+        # to_hex is made available by ${ZEPHYR_BASE}/cmake/hex.cmake
+        to_hex(${NCS_VERSION_CODE} NCS_VERSION_NUMBER)
 
-configure_file(${NRF_DIR}/ncs_version.h.in ${ZEPHYR_BINARY_DIR}/include/generated/ncs_version.h)
+        configure_file(${NRF_DIR}/ncs_version.h.in ${ZEPHYR_BINARY_DIR}/include/generated/ncs_version.h)
+endif()
